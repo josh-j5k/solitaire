847b0db8700a1e1d791bc7ba32d1058edd20348e
josh-j5k <joshdejong5k@gmail.com>
2024-03-12 22:03:40 -0700

    new feature

--- src/App.svelte
+++ src/App.svelte
@@ -29,11 +29,6 @@
 		seconds: 0,
 	}
 
-	let score = 0
-	let streak = 5
-
-	let win = false
-	let gameStarted = false
 	const successAudio = new Audio("/src/assets/audio/success_H3LxEVI6.m4a")
 	const dealCard = new Audio("/src/assets/audio/Reveal Card.m4a")
 	const dragStartSound = new Audio(
@@ -53,6 +48,9 @@
 	)
 	const winningSound = new Audio(
 		"/src/assets/audio/positive-notification-new-level-152480.mp3"
+	)
+	const shuffleSound = new Audio(
+		"/src/assets/audio/Solitaire Card shuffle SFX - Page 2.m4a"
 	)
 	const arr = [
 		"King",
@@ -76,7 +74,7 @@
 			components: components.map((component) => component),
 		}
 	})
-
+	const height = 180
 	let Cards = <Array<card>>[]
 	cardTypeWithComponent.forEach((card) => {
 		card.components.forEach((component) => {
@@ -87,9 +85,7 @@
 			Cards.push(obj)
 		})
 	})
-
-	let stockPile = <Array<card>>[]
-	let tableau = <Array<row>>[
+	const emptyTableau = <Array<row>>[
 		{
 			faceDown: [],
 			faceUp: <Array<card>>[],
@@ -119,15 +115,22 @@
 			faceUp: <Array<card>>[],
 		},
 	]
-	let foundation = <TFoundation>{
+	const emptyFoundation = <TFoundation>{
 		0: <Array<card>>[],
 		1: <Array<card>>[],
 		2: <Array<card>>[],
 		3: <Array<card>>[],
 	}
+	let stockPile = <Array<card>>[]
+	let tableau = [...emptyTableau]
+	let foundation = emptyFoundation
 
-	const height = 180
-
+	let score = 0
+	let streak = 5
+	let gameLoadingAnimation = true
+	let win = false
+	let pause = false
+	let gameStarted = false
 	let activeCard: string
 	let activeCardElement: HTMLElement
 	let activeCardNumber: number
@@ -137,7 +140,7 @@
 	let isDraggedFromWastePile = false
 	let top: string
 	let left: string
-
+	let menuToggled = false
 	let mouseX: number
 	let mouseY: number
 	let totalCards = 52
@@ -185,13 +188,52 @@
 			setCardFaceDown(index)
 		}
 
-		setTimeout(() => {
+		let startGameTimeout = setTimeout(() => {
+			let index = 1
+			let parentIndex = 1
+			let startingParentIndex = 1
 			const containingBlock = document.querySelectorAll(".containing_block")
 
 			containingBlock.forEach((ele) => {
 				const block = ele as HTMLDivElement
 				alignElements(block)
 			})
+			shuffleSound.play()
+			containingBlock[0].children[1].classList.add("face-up-first-animation")
+			let animationInterval = setInterval(() => {
+				if (parentIndex > containingBlock.length - 1) {
+					index++
+					containingBlock[startingParentIndex].children[index].classList.add(
+						"face-up-animation"
+					)
+					startingParentIndex++
+					parentIndex = startingParentIndex
+				} else {
+					containingBlock[parentIndex].children[index].classList.add(
+						"face-down-animation"
+					)
+					parentIndex++
+				}
+				if (index > 6) {
+					gameLoadingAnimation = false
+					containingBlock[0].children[1].classList.remove(
+						"face-up-first-animation"
+					)
+					containingBlock.forEach((ele) => {
+						for (let index = 0; index < ele.children.length; index++) {
+							const element = ele.children[index]
+							if (element.classList.contains("face-down-animation")) {
+								element.classList.remove("face-down-animation")
+							}
+							if (element.classList.contains("face-up-animation")) {
+								element.classList.remove("face-up-animation")
+							}
+						}
+					})
+					clearInterval(animationInterval)
+					clearTimeout(startGameTimeout)
+				}
+			}, 50)
 		}, 5)
 		timeInterval = setInterval(() => {
 			time.seconds++
@@ -200,6 +242,63 @@
 				time.minutes += 1
 			}
 		}, 1000)
+	}
+	function startNewGame() {
+		clickSound.play()
+		stockPile = <Array<card>>[]
+		tableau.forEach((ele) => {
+			ele.faceDown = <Array<card>>[]
+			ele.faceUp = <Array<card>>[]
+		})
+		Object.values(foundation).forEach((val) => (val = <Array<card>>[]))
+		tableau = tableau
+		foundation = foundation
+		clearInterval(timeInterval)
+		menuToggled = false
+		setTimeout(() => {
+			for (let index = 0; index < 7; index++) {
+				let randomNumber = Math.floor(Math.random() * totalCards)
+				tableau[index].faceUp.push(Cards[randomNumber])
+				Cards.splice(randomNumber, 1)
+				totalCards -= 1
+			}
+
+			Cards.sort(() => (Math.random() > 0.5 ? 1 : -1))
+			for (let index = 1; index < 7; index++) {
+				setCardFaceDown(index)
+			}
+			tableau = tableau
+			stockPile = stockPile
+			const containingBlock = document.querySelectorAll(".containing_block")
+
+			containingBlock.forEach((ele) => {
+				const block = ele as HTMLDivElement
+				alignElements(block)
+			})
+		}, 2000)
+	}
+	function pauseAndPlayGame() {
+		clickSound.play()
+		pause = !pause
+		if (pause) {
+			clearInterval(timeInterval)
+			clearInterval(streakInterval)
+		} else {
+			timeInterval = setInterval(() => {
+				time.seconds++
+				if (time.seconds > 59) {
+					time.seconds = 0
+					time.minutes += 1
+				}
+			}, 1000)
+			streakInterval = setInterval(() => {
+				streak--
+				if (streak < 1) {
+					streak = 5
+					clearInterval(streakInterval)
+				}
+			}, 1000)
+		}
 	}
 	function newGame() {
 		clickSound.play()
@@ -658,7 +757,13 @@
 			<div class="w-5/6 mx-auto h-full flex items-center justify-between">
 				<div class="w-full">
 					<button
-						class="w-8 aspect-square flex justify-center items-center bg-[#00000083]"
+						on:click={() => {
+							clickSound.play()
+							menuToggled = true
+						}}
+						class="w-8 aspect-square flex justify-center items-center bg-[#00000083] transition-opacity {menuToggled
+							? 'opacity-0'
+							: 'opacity-100'}"
 					>
 						<svg
 							viewBox="0 -1 12 12"
@@ -698,6 +803,167 @@
 					</div>
 				</div>
 			</div>
+		</div>
+		<div
+			class="w-1/4 h-screen fixed top-0 flex flex-col items-center gap-8 left-0 py-24 bg-[#000000dc] z-50 transition-transform duration-150 {menuToggled
+				? 'translate-x-0'
+				: '-translate-x-full'}"
+		>
+			<button
+				on:click={() => {
+					clickSound.play()
+					menuToggled = false
+				}}
+				class="absolute top-6 right-4"
+			>
+				<span>
+					<svg
+						viewBox="0 0 24 24"
+						class="w-8 h-8"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+							id="SVGRepo_tracerCarrier"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						></g><g id="SVGRepo_iconCarrier">
+							<path
+								d="M7.75716 7.75736C8.14768 7.36683 8.78084 7.36683 9.17137 7.75736L11.9998 10.5858L14.8283 7.75736C15.2188 7.36684 15.852 7.36684 16.2425 7.75736C16.6331 8.14789 16.6331 8.78105 16.2425 9.17158L13.4141 12L16.2424 14.8284C16.633 15.2189 16.633 15.8521 16.2424 16.2426C15.8519 16.6332 15.2187 16.6332 14.8282 16.2426L11.9998 13.4143L9.17146 16.2426C8.78094 16.6332 8.14777 16.6332 7.75725 16.2426C7.36672 15.8521 7.36672 15.219 7.75725 14.8284L10.5856 12L7.75716 9.17157C7.36663 8.78104 7.36663 8.14788 7.75716 7.75736Z"
+								fill="#fff"
+							></path>
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3Z"
+								fill="#fff"
+							></path>
+						</g></svg
+					>
+				</span>
+			</button>
+			<button on:click={startNewGame} class="flex gap-4 group">
+				<span>
+					<svg
+						viewBox="0 0 32 32"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						class="w-10 h-10 group-hover:scale-90 transition-transform"
+						fill="#000000"
+						><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+							id="SVGRepo_tracerCarrier"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						></g><g id="SVGRepo_iconCarrier">
+							<title>plus</title> <desc>Created with Sketch Beta.</desc>
+							<defs> </defs>
+							<g
+								id="Page-1"
+								stroke="none"
+								stroke-width="1"
+								fill="none"
+								fill-rule="evenodd"
+							>
+								<g
+									id="Icon-Set-Filled"
+									transform="translate(-362.000000, -1037.000000)"
+									fill="#00a3f5"
+								>
+									<path
+										d="M390,1049 L382,1049 L382,1041 C382,1038.79 380.209,1037 378,1037 C375.791,1037 374,1038.79 374,1041 L374,1049 L366,1049 C363.791,1049 362,1050.79 362,1053 C362,1055.21 363.791,1057 366,1057 L374,1057 L374,1065 C374,1067.21 375.791,1069 378,1069 C380.209,1069 382,1067.21 382,1065 L382,1057 L390,1057 C392.209,1057 394,1055.21 394,1053 C394,1050.79 392.209,1049 390,1049"
+										id="plus"
+									>
+									</path>
+								</g>
+							</g>
+						</g></svg
+					>
+				</span>
+				<span
+					class="capitalize text-3xl font-bold text-white group-hover:text-[#00a3f5]"
+					>new game</span
+				>
+			</button>
+			<button on:click={pauseAndPlayGame} class=" flex gap-4 group">
+				<span>
+					{#if pause}
+						<svg
+							viewBox="0 0 24 24"
+							fill="none"
+							class="w-10 h-10 group-hover:scale-90 transition-transform"
+							xmlns="http://www.w3.org/2000/svg"
+							><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+								id="SVGRepo_tracerCarrier"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							></g><g id="SVGRepo_iconCarrier">
+								<path
+									d="M3 12L3 18.9671C3 21.2763 5.53435 22.736 7.59662 21.6145L10.7996 19.8727M3 8L3 5.0329C3 2.72368 5.53435 1.26402 7.59661 2.38548L20.4086 9.35258C22.5305 10.5065 22.5305 13.4935 20.4086 14.6474L14.0026 18.131"
+									stroke="#00a3f5"
+									stroke-width="1.5"
+									stroke-linecap="round"
+								></path>
+							</g></svg
+						>
+					{:else}
+						<svg
+							viewBox="0 0 24 24"
+							class="w-10 h-10 group-hover:scale-90 transition-transform"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+								id="SVGRepo_tracerCarrier"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							></g><g id="SVGRepo_iconCarrier">
+								<path
+									d="M2 18C2 19.8856 2 20.8284 2.58579 21.4142C3.17157 22 4.11438 22 6 22C7.88562 22 8.82843 22 9.41421 21.4142C10 20.8284 10 19.8856 10 18V6C10 4.11438 10 3.17157 9.41421 2.58579C8.82843 2 7.88562 2 6 2C4.11438 2 3.17157 2 2.58579 2.58579C2 3.17157 2 4.11438 2 6V14"
+									stroke="#00a3f5"
+									stroke-width="1.5"
+									stroke-linecap="round"
+								></path>
+								<path
+									d="M22 6C22 4.11438 22 3.17157 21.4142 2.58579C20.8284 2 19.8856 2 18 2C16.1144 2 15.1716 2 14.5858 2.58579C14 3.17157 14 4.11438 14 6V18C14 19.8856 14 20.8284 14.5858 21.4142C15.1716 22 16.1144 22 18 22C19.8856 22 20.8284 22 21.4142 21.4142C22 20.8284 22 19.8856 22 18V10"
+									stroke="#00a3f5"
+									stroke-width="1.5"
+									stroke-linecap="round"
+								></path>
+							</g></svg
+						>
+					{/if}
+				</span>
+				<span
+					class="capitalize text-3xl font-bold text-white group-hover:text-[#00a3f5]"
+				>
+					{pause ? "continue game" : "pause game"}
+				</span>
+			</button>
+			<button class="flex gap-4 group">
+				<span>
+					<svg
+						class="w-10 h-10 group-hover:scale-90 transition-transform"
+						fill="#00a3f5"
+						viewBox="0 0 32 32"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+							id="SVGRepo_tracerCarrier"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						></g><g id="SVGRepo_iconCarrier">
+							<title>recycle</title>
+							<path
+								d="M15.966 1.232c-0.97 0.021-1.901 0.261-2.807 0.655l1.404 2.62c0.463-0.165 0.937-0.277 1.404-0.281 1.599 0 3.292 0.853 4.585 3.088l5.333 9.264 1.778-2.901-4.491-7.86c-1.714-2.964-4.434-4.608-7.205-4.585 0 0 0 0 0 0zM7.171 8.811l-4.117 7.205c-1.569 2.715-1.68 5.792-0.374 8.234 0.541 1.012 1.362 1.842 2.339 2.526l1.497-2.526c-0.502-0.397-0.929-0.867-1.216-1.403-0.781-1.46-0.831-3.41 0.281-5.334l5.053-8.702h-3.462zM27.194 21.536c-0.098 0.48-0.239 0.896-0.468 1.311-0.801 1.448-2.372 2.526-4.866 2.526h-10.199l1.778 2.994h8.421c3.402 0 6.146-1.601 7.486-4.023 0.472-0.855 0.732-1.824 0.842-2.807l-2.994 0zM11.272 12.6l-6.728-3.879 6.728-3.879-0 7.759zM22.219 15.005l6.728 3.879 0-7.759-6.729 3.879zM8.193 26.81l6.728-3.879 0 7.759-6.729-3.879z"
+							></path>
+						</g></svg
+					>
+				</span>
+				<span
+					class="capitalize text-3xl font-bold text-white group-hover:text-[#00a3f5]"
+				>
+					change difficulty
+				</span>
+			</button>
 		</div>
 		<div class="w-4/5 mx-auto grid gap-4 gap-y-12 grid-cols-7">
 			<div class="relative col-start-1 col-end-2 row-start-1 row-end-2">
@@ -866,7 +1132,7 @@
 			</div>
 			<div
 				data-foundation="0"
-				class="relative {`h-[${height}px]`} col-start-4 col-end-5 row-start-1 row-end-2 border-2 rounded-xl border-gray-100"
+				class="relative {`h-[180px]`} col-start-4 col-end-5 row-start-1 row-end-2 border-2 rounded-xl border-gray-100"
 			>
 				<div
 					on:dragover={dragOver}
@@ -1065,7 +1331,8 @@
 						on:dragstart={dragStart}
 						on:drag={drag}
 						on:dragend={dragEnd}
-						class="{dimensions} {design}"
+						class="{dimensions} {design} {gameLoadingAnimation &&
+							'-translate-y-36'}"
 					>
 						<Placeholder {card} />
 					</div>
@@ -1083,7 +1350,10 @@
 				>
 				</div>
 				{#each tableau[1].faceDown as _, index}
-					<div class="{dimensions} cursor-default">
+					<div
+						class="{dimensions} cursor-default {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'}"
+					>
 						<CardFaceDown {dimensions} />
 					</div>
 				{/each}
@@ -1098,7 +1368,8 @@
 						on:dragstart={dragStart}
 						on:drag={drag}
 						on:dragend={dragEnd}
-						class="{dimensions} {design}"
+						class="{dimensions} {design} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'} "
 					>
 						<Placeholder {card} />
 					</div>
@@ -1116,7 +1387,11 @@
 				>
 				</div>
 				{#each tableau[2].faceDown as _, index}
-					<div id={"_two" + index} class="{dimensions} cursor-default">
+					<div
+						id={"_two" + index}
+						class="{dimensions} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'} cursor-default"
+					>
 						<CardFaceDown {dimensions} />
 					</div>
 				{/each}
@@ -1131,7 +1406,8 @@
 						on:dragstart={dragStart}
 						on:drag={drag}
 						on:dragend={dragEnd}
-						class="{dimensions} {design}"
+						class="{dimensions} {design} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'}"
 					>
 						<Placeholder {card} />
 					</div>
@@ -1149,7 +1425,11 @@
 				>
 				</div>
 				{#each tableau[3].faceDown as _, index}
-					<div id={"_three" + index} class="{dimensions} cursor-default">
+					<div
+						id={"_three" + index}
+						class="{dimensions} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'} cursor-default"
+					>
 						<CardFaceDown {dimensions} />
 					</div>
 				{/each}
@@ -1164,7 +1444,8 @@
 						on:dragstart={dragStart}
 						on:drag={drag}
 						on:dragend={dragEnd}
-						class="{dimensions} {design}"
+						class="{dimensions} {design} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'}"
 					>
 						<Placeholder {card} />
 					</div>
@@ -1182,7 +1463,11 @@
 				>
 				</div>
 				{#each tableau[4].faceDown as _, index}
-					<div id={"_four" + index} class="{dimensions} cursor-default">
+					<div
+						id={"_four" + index}
+						class="{dimensions} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'} cursor-default"
+					>
 						<CardFaceDown {dimensions} />
 					</div>
 				{/each}
@@ -1197,7 +1482,8 @@
 						on:dragstart={dragStart}
 						on:drag={drag}
 						on:dragend={dragEnd}
-						class="{dimensions} {design}"
+						class="{dimensions} {design} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'}"
 					>
 						<Placeholder {card} />
 					</div>
@@ -1214,7 +1500,11 @@
 					class="absolute w-full h-[inherit] inset-0 opacity-0 dragover_zone"
 				></div>
 				{#each tableau[5].faceDown as _, index}
-					<div id={"_five" + index} class="{dimensions} cursor-default">
+					<div
+						id={"_five" + index}
+						class="{dimensions} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'} cursor-default"
+					>
 						<CardFaceDown {dimensions} />
 					</div>
 				{/each}
@@ -1229,7 +1519,8 @@
 						on:dragstart={dragStart}
 						on:drag={drag}
 						on:dragend={dragEnd}
-						class="{dimensions} {design}"
+						class="{dimensions} {design} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'}"
 					>
 						<Placeholder {card} />
 					</div>
@@ -1247,7 +1538,11 @@
 				>
 				</div>
 				{#each tableau[6].faceDown as _, index}
-					<div id={"_six" + index} class="{dimensions} cursor-default">
+					<div
+						id={"_six" + index}
+						class="{dimensions} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'} cursor-default"
+					>
 						<CardFaceDown {dimensions} />
 					</div>
 				{/each}
@@ -1262,7 +1557,8 @@
 						on:dragstart={dragStart}
 						on:drag={drag}
 						on:dragend={dragEnd}
-						class="{dimensions} {design}"
+						class="{dimensions} {design} {gameLoadingAnimation &&
+							'-translate-y-36 -translate-x-24 opacity-0'}"
 					>
 						<Placeholder {card} />
 					</div>
@@ -1355,10 +1651,47 @@
 		box-shadow: 0 0 4px 5px rgba(0, 204, 255, 0.733);
 		animation: valid-move 1000ms ease-out forwards;
 	}
+	.face-up-animation {
+		animation: flip-translate 200ms ease forwards;
+	}
+	.face-up-first-animation {
+		animation: flip-first-translate 200ms ease forwards;
+	}
+	.face-down-animation {
+		animation: translate-y 200ms ease forwards;
+	}
 	@keyframes reveal-card {
 		to {
 			transform: translateX(0);
 			opacity: 100;
+		}
+	}
+	@keyframes translate-y {
+		from {
+			transform: translate(-5rem, -9rem);
+			opacity: 1;
+		}
+		to {
+			transform: translate(0);
+			opacity: 1;
+		}
+	}
+	@keyframes flip-translate {
+		from {
+			transform: translate(-5rem, -9rem) rotateY(90deg);
+			opacity: 1;
+		}
+		to {
+			transform: translate(0) rotateY(0);
+			opacity: 1;
+		}
+	}
+	@keyframes flip-first-translate {
+		from {
+			transform: translateY(-9rem) rotateY(90deg);
+		}
+		to {
+			transform: translateY(0) rotateY(0);
 		}
 	}
 	@keyframes valid-move {
